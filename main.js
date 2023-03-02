1fc3e4abf38ac438f3719e2143a3dda1037e367c
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-02 17:17:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -20,3 +20,34 @@ recognition.onresult = function(event) {
 var content = event.results[0][0].transcript;
 
  
+//???
+
+function setup() {
+  canvas = createCanvas(900, 600);
+}
+
+function draw() {
+  if(drawCircle == "set")
+  {
+    radius = Math.floor(Math.random() * 100);
+    circle(x, y, radius);
+    document.getElementById("status").innerHTML = "UCírculo desenhado. ";
+    drawCircle = "";
+  }
+
+  if(drawRect == "set")
+  {
+    rect(x,y,70,50);
+    document.getElementById("status").innerHTML = "Retângulo desenhado. ";
+    drawRect = "";
+  }
+
+}
+
+
+
+
+
+
+
+
